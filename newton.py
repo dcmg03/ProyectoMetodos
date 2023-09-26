3c375a25e2127bb863fd74c49cd3dc5ffd8044c3
Camillows98 <williamlopez0072014@gmail.com>
2023-09-26 17:34:33 -0500

    Prueba newton

--- newton.py
+++ newton.py
@@ -27,7 +27,17 @@ def gPrima(fdx,dfx,ddfx):
     gPrima = abs(1-((dfx*dfx)-(fdx*ddfx))/dfx**2)
     return gPrima
 
-def newton_raphson_method(funcion, derivfun, x0, err, iter):
+def newton_raphson_method(funcion, derivfun, xx0, err, iter):
+    
+    a = -2  
+    b = 5  
+    n = iter
+    x0 = xx0
+    yn = funcion
+    xn = np.linspace(a, b, n)  
+    x = np.linspace(-6,4,100);
+    
+    
     plt.plot(xn, yn)
     plt.grid(True)
     plt.axhline(0, color="#ff0000") 
@@ -36,13 +46,6 @@ def newton_raphson_method(funcion, derivfun, x0, err, iter):
     plt.ylabel("Eje Y")
     plt.xlabel("Eje X")
     plt.plot(xi,0, 'ro')
-    a = -2  
-    b = 5  
-    xn = np.linspace(a, b, n)  
-    n = iter
-    x = np.linspace(-6,4,100);
-    x0 = 1
-    yn = funcion
     error = 0.00010
     # PROCEDIMIENTO
     tramo = abs(2*error)
@@ -50,18 +53,17 @@ def newton_raphson_method(funcion, derivfun, x0, err, iter):
     tabla=[]
     iteracion=-1
     while (tramo>=error):
-        iteracion=iteracion+1
-        xnuevo = xi - (funcionNormal(xi)/primeraDeriv(xi))
-        tramo  = abs(xnuevo-xi)
-        gPrimaValor = gPrima(funcionNormal(xi),primeraDeriv(xi),segundaDeriv(xi))
-        xi = xnuevo
-        tabla.append([iteracion,xi,tramo,gPrimaValor])
-    # SALIDA
-    #print(tabulate(tabla, headers=['Iteración','Xi','Xi+1 - Xi','g(x)´']))
-    print("\nLa raíz exacta es: ", xi,"\n\n")
-    # GRÁFICA
+            iteracion=iteracion+1
+            xnuevo = xi - (funcionNormal(xi)/primeraDeriv(xi))
+            tramo  = abs(xnuevo-xi)
+            gPrimaValor = gPrima(funcionNormal(xi),primeraDeriv(xi),segundaDeriv(xi))
+            xi = xnuevo
+            tabla.append([iteracion,xi,tramo,gPrimaValor])
+        # SALIDA
+        #print(tabulate(tabla, headers=['Iteración','Xi','Xi+1 - Xi','g(x)´']))
+        #print("\nLa raíz exacta es: ", xi,"\n\n")
+        # GRÁFICA
+    return plt.gcf(), xi, err, error, tramo
 
 
-
-
-plt.show()
+#plt.show()
